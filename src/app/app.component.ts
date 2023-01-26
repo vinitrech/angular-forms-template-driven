c3bce24338b3e09204510f164110019ec3d1a46e
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-01-26 14:53:23 -0300

    finishign Template Driven approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,38 @@ import {NgForm} from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild('form') signupForm: NgForm;
+  answer: string = '';
+  genders: string[] = ["male", "female"];
+  submitted: boolean = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // Set whole form controls' values
+
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    // Change only parts of the form controls
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(form: NgForm) {
@@ -19,6 +48,14 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset(); // resets the form completely, including its watchers (invalid, untouched)
   }
 }
